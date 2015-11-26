c76db62e6343b3c89d7ba4abb89d491dea676b48
mmazaev <mazaev.maxim@gmail.com>
2015-11-26 09:26:38 +0300

    fix keyerror in comments

--- instagram/models.py
+++ instagram/models.py
@@ -96,8 +96,9 @@ class Media(ApiModel):
 
         new_media.comment_count = entry['comments']['count']
         new_media.comments = []
-        for comment in entry['comments']['data']:
-            new_media.comments.append(Comment.object_from_dictionary(comment))
+        if 'data' in entry['comments']:
+            for comment in entry['comments']['data']:
+                new_media.comments.append(Comment.object_from_dictionary(comment))
 
         new_media.users_in_photo = []
         if entry.get('users_in_photo'):
